a7ba81687ce8c50c83068ec5a050b1126a51e51f
suryarajb <surya@trueconnect.co.in>
2021-10-01 08:30:26 +0530

    Fixed the feedback

--- product/src/App.js
+++ product/src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import history from "./history";
 import Test from "./test";
 import Test1 from "./test2";
-import ButtonComponent from "./Components/Inputfolder/Button/Button";
-
 
 function App() {
   return (
     <div className="App">
-      <ButtonComponent color="success" label="Hello" />
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Test} />
+          <Route exact path="/test" component={Test1} />
+        </Switch>
+      </Router>
     </div>
   );
 }
